5212c74f49ebab82627fcda1d8a8c133493620a0
Jacob Miller <dev@jacobmiller22.com>
2025-02-23 16:03:50 -0500

    backup using volback

--- vaultwarden/backup/backup.sh
+++ vaultwarden/backup/backup.sh
@@ -2,12 +2,14 @@ echo "Running backup!!"
 
 
 echo "Backing up sqlite db!"
-sqlite3 /tmp/vw-data/db.sqlite3 ".backup './vw-db-backup.sqlite3'"
+sqlite3 /tmp/vw-data/db.sqlite3 ".backup '/app/backups/vw-db-backup.sqlite3'"
 echo "Backup created!"
 
+echo "$(pwd)"
+echo "$(ls)"
 echo "Saving backup to cloud!"
-./volback \
-	--path=/Users/jacobmiller22/Documents/bjorn-coolify.env \
+/app/volback \
+	--path="/app/backups/vw-db-backup.sqlite3" \
 	--dest.kind="s3" \
 	--dest.prefix="backups/vw" \
 	--dest.endpoint="${BACKUP_DEST_ENDPOINT}" \
@@ -16,5 +18,8 @@ echo "Saving backup to cloud!"
 	--dest.secret-access-key="${BACKUP_DEST_SECRET_ACCESS_KEY}" \
 	--enc.key="${BACKUP_ENCRYPTION_KEY}" 
 echo "Backup saved to cloud!"
+
+echo "Cleaning up!"
+rm "/app/backups/vw-db-backup.sqlite3"
 
 echo "Finished running backup!!"
