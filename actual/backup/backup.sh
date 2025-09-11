af559742ab3b6dcb4f37307a2b1e545009d21c98
Jacob Miller <dev@jacobmiller22.com>
2025-09-10 21:41:11 -0400

    feat: actual backup

---
+++ actual/backup/backup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Running backup!!"
+
+echo "Sourcing secrets"
+source /run/secrets/env_vars
+
+target_path="/data"
+backup_filename="actual-data-backup-$(date +'%Y-%m-%d_%H-%M-%S').zip"
+destination_path="backups/actual/data-backup.zip.enc"
+
+backup_path="/app/backups/${backup_filename}"
+backup_parent_path=$(dirname "$backup_path")
+
+echo "Backing up sqlite db at ${target_path} to ${backup_path}"
+
+mkdir -p $backup_parent_path
+sqlite3 "$target_path" ".backup '$backup_path'"
+chmod 777 "$backup_path"
+
+echo "Backup created!"
+
+echo "Saving backup to cloud!"
+/app/volback \
+	--src.kind="fs" \
+	--src.path="$backup_path" \
+	--enc.key="${BACKUP_ENCRYPTION_KEY}"  \
+	--dst.kind="s3" \
+	--dst.path="${destination_path}" \
+	--dst.s3-endpoint="${BACKUP_DEST_ENDPOINT}" \
+	--dst.s3-bucket="${BACKUP_DEST_BUCKET}" \
+	--dst.s3-access-key-id="${BACKUP_DEST_ACCESS_KEY_ID}" \
+	--dst.s3-secret-access-key="${BACKUP_DEST_SECRET_ACCESS_KEY}" \
+	--dst.s3-region="us-east-1" 
+
+echo "Backup saved to cloud!"
+
+echo "Cleaning up!"
+rm $backup_path
+
+echo "Finished running backup!"
---
+++ actual/backup/entrypoint.sh
@@ -0,0 +1,16 @@
+set -e
+
+mkdir -p /run/secrets
+# Write only the secrets you need into a file
+cat <<EOF > /run/secrets/env_vars
+BACKUP_DEST_ENDPOINT="${BACKUP_DEST_ENDPOINT}"
+BACKUP_DEST_BUCKET="${BACKUP_DEST_BUCKET}"
+BACKUP_DEST_ACCESS_KEY_ID="${BACKUP_DEST_ACCESS_KEY_ID}"
+BACKUP_DEST_SECRET_ACCESS_KEY="${BACKUP_DEST_SECRET_ACCESS_KEY}"
+BACKUP_ENCRYPTION_KEY="${BACKUP_ENCRYPTION_KEY}"
+EOF
+
+# Optionally, secure the file permissions
+chmod 600 /run/secrets/env_vars
+
+cron -f
